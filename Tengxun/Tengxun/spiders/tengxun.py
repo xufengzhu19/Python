e70e8171cfb8f19a0e58284001bf3c01d876039c
xufengzhu19 <304882553@qq.com>
2019-01-07 11:51:34 +0800

    2019年1月7日

--- Tengxun/Tengxun/spiders/tengxun.py
+++ Tengxun/Tengxun/spiders/tengxun.py
@@ -1,34 +1,25 @@
 # -*- coding: utf-8 -*-
 import scrapy
+# 导入提取页面连接的类
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
-from Tengxun.items import TengxunItem
 
+class TengxunSpider(CrawlSpider):
+    name = 'tengxun'
+    allowed_domains = ['hr.tencent.com']
+    start_urls = ['http://hr.tencent.com/']
 
-class TengxunSpider(scrapy.Spider):
-    name = "tengxun"
-    allowed_domains = ["hr.tencent.com"]
-    url = 'https://hr.tencent.com/position.php?&start='
-    start_urls = [url + str(0)]
+    rules = (
+        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+    )
 
-    def parse(self, response):
-        # 284页url地址给调度器入队列
-        for i in range(0, 2831, 10):
-            url=self.url + str(i)
-            yield scrapy.Request(url, callback=self.parseHtml)
+    def parse_item(self, response):
+        i = {}
+        #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
+        #i['name'] = response.xpath('//div[@id="name"]').extract()
+        #i['description'] = response.xpath('//div[@id="description"]').extract()
+        return i
 
-    def parseHtml(self, response):
-        item = TengxunItem()
-        baseList = response.xpath('//tr[@class="even"]|//tr[@class="odd"]')
-        for base in baseList:
-            item['jobName'] = base.xpath('./td[1]/a/text()').extract()[0]
-            item['jobCategory'] = base.xpath('./td[2]/text()').extract()
-            if item['jobCategory']:
-                item['jobCategory']=item['jobCategory'][0]
-            else:
-                item['jobCategory']='无'
-            item['jobNumber'] = base.xpath('./td[3]/text()').extract()[0]
-            item['jobAddress'] = base.xpath('./td[4]/text()').extract()[0]
-            item['pubTime'] = base.xpath('./td[5]/text()').extract()[0]
-            item['jobLink'] = base.xpath('./td[1]/a/text()').extract()[0]
+    def parseTencent(self,response):
 
-            yield item
